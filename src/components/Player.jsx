a215f016396939f45ede04f7cf9848dd1185e24c
braverokmc79 <braverokmc79@gmail.com>
2024-03-02 14:04:12 +0900

    first commit

---
+++ src/components/Player.jsx
@@ -0,0 +1,11 @@
+export default function Player() {
+  return (
+    <section id="player">
+      <h2>환영합니다.</h2>
+      <p>
+        <input type="text" />
+        <button>이름 설정</button>
+      </p>
+    </section>
+  );
+}
